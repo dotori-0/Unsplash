8a60372c063f1cf7f44f8a0ce1fd3b273ab610da
s <sesan131@gmail.com>
2022-10-28 18:29:07 +0900

    Initial Commit

---
+++ Unsplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash
+//
+//  Created by SC on 2022/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
